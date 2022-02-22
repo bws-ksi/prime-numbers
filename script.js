19cb4c875356b2e7ceb84bbaee4731cfb73db74a
bws.ksi-DMRKT <bws.ksi@gmail.com>
2022-02-22 15:40:48 +0200

    last changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const x = 100 //перебираем цифры до 100 
+ 
 
 nextNumber:
-for (let i = 2; i <= x; i++){
+for (let i = 2; i <= 100; i++){
   for (let j = 2; j < i; j++) {
     if (i % j === 0) continue nextNumber; //при переборе деления на предыдущих чисел как только делится без остатка то это не простое число, берем следующее
   }
-  console.log(i);
+  console.log(i); 
 }
